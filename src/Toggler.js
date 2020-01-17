ef3ce86ec1e4385813beaeba3f28a355ace6c539
kdord <katya.koliada49@gmail.com>
2020-01-17 10:41:04 +0200

    buolding a custom hook: useToggleState

---
+++ src/Toggler.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import useToggle from './hooks/useToggle';
+
+function Toggler() {
+  const [isHappy, toggleIsHappy] = useToggle(true);
+  const [isHeartBroken, toggleIsHeartBroken] = useToggle(false);
+
+  return (
+    <div>
+      <h1 onClick={toggleIsHappy}>{isHappy ? '😀' : '😓'}</h1>;
+      <h1 onClick={toggleIsHeartBroken}>{isHeartBroken ? '💔' : '💓'}</h1>;
+    </div>
+  );
+}
+export default Toggler;
